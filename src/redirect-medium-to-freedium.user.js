b43dd05583de307df27853e262d5778fe54e2046
mangogan-git <mangogan.git@gmail.com>
2024-12-05 15:46:58 +0800

    feat: Update redirect-medium-to-freedium.user.js
    adjust the implementation method

--- src/redirect-medium-to-freedium.user.js
+++ src/redirect-medium-to-freedium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Redirect Medium to FreeMedium
 // @description  Redirect medium.com to freedium.cfd so you can read without login
-// @version      0.1
+// @version      0.2
 // @license      MIT
 // @website      https://github.com/mangogan-git/tempermonkey-scripts
 // @source       https://github.com/mangogan-git/tampermonkey-scripts/raw/master/src/redirect-medium-to-freedium.user.js
@@ -16,8 +16,9 @@
     'use strict';
 
     const oldHref = location.href;
+    // handle "Go to the original"
     if (oldHref.endsWith('#bypass')) return;
 
-    const newHref = oldHref.replace('https://', 'https://freedium.cfd/');
+    const newHref = 'https://freedium.cfd/medium.com' + location.pathname + location.search;
     location.href = newHref;
 })();
