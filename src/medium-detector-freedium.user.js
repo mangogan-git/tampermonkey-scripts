63e0750f9e3d081970ccf11ca7a1025d2a17e0f5
mangogan-git <mangogan.git@gmail.com>
2025-11-27 21:15:55 +0800

    Update version to 1.4.1 and fix URL formatting

--- src/medium-detector-freedium.user.js
+++ src/medium-detector-freedium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Medium Detector - Freedium Redirect
 // @description  Detect Medium articles and show dialog to redirect to Freedium
-// @version      1.4
+// @version      1.4.1
 // @license      MIT
 // @author       mangogan-git
 // @website      https://github.com/mangogan-git/tampermonkey-scripts
@@ -209,7 +209,7 @@
 
     function handleConfirm() {
         const currentUrl = window.location.href;
-        const freediumUrl = 'https://freedium-mirror.cfd//' + currentUrl;
+        const freediumUrl = 'https://freedium-mirror.cfd/' + currentUrl;
         window.location.href = freediumUrl;
     }
 
